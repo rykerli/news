8eaa3d30f64077ac5f2569162e14558c6b4bf6d0
rykerli <rykerli@tencent.com>
2020-12-09 12:46:10 +0800

    feature:介入词资源词汇统计

--- lexicostatistics/interventional_resource_vocabulary.py
+++ lexicostatistics/interventional_resource_vocabulary.py
@@ -1,4 +1,201 @@
 # -*- encoding: utf-8 -*-
+import os
+import sys
+import re
+
+sys.path.append("../utils")  # noqa
+import sql_util as sql  # noqa
+from xlwt_util import *  # noqa
+
 """
 介入资源词汇统计
 """
+
+
+def get_data():
+    sina_data = sql.queryall(
+        "select number, pos_status, neg_status, remove_useless, post_content_txt "
+        "from sina where pos_status=0 or neg_status = 0"
+    )
+    sohu_data = sql.queryall(
+        "select number, negative_number, pos_status, neg_status, remove_useless, article_content_txt "
+        "from sohu where pos_status = 0 or neg_status = 0"
+    )
+    tianya_data = sql.queryall(
+        "select number, negative_number, pos_status, neg_status, remove_useless, question_detail "
+        "from tianya where pos_status = 0 or neg_status = 0"
+    )
+    return {"sina": sina_data, "sohu": sohu_data, "tianya": tianya_data}
+
+
+def get_txt_data():
+    with open("../resource/vocabulary/intervention_word.txt", "r") as f:
+        txt_data = f.read()
+    result = [item for item in txt_data.split("\n")]
+    return result
+
+
+def count_word(data, intervention_word):
+    """
+    词汇统计
+    :parm data
+    :return:
+    """
+    all_data = {}
+    result = []
+    intervention_word_copy = intervention_word[:]
+    for sina in data["sina"]:
+        sina_data = [sina["number"], sina["post_content_txt"]]
+        intervention_word_result = []
+        re_result = []
+        for i, word in enumerate(intervention_word_copy):
+            if "......" in word:
+                re_data = re.findall(
+                    word.replace("......", r"\w.{1,5}?"), sina["remove_useless"]
+                )
+            else:
+                re_data = re.findall(word, sina["remove_useless"])
+            if re_data:
+                re_result.extend(re_data)
+                intervention_word_result.append(intervention_word[i])
+        write_html_file(sina["number"], sina["post_content_txt"], re_result)
+        sina_data.extend(
+            [
+                ",".join(list(set(intervention_word_result))),
+                len(list(set(intervention_word_result))),
+                len(intervention_word_result),
+                get_status(sina["pos_status"], sina["neg_status"]),
+                "./html_file/{}.html".format(sina["number"]),
+            ]
+        )
+        result.append(sina_data)
+    all_data.update({"sina": result})
+
+    intervention_word_copy = intervention_word[:]
+    for sohu in data["sohu"]:
+        sohu_data = [sohu["number"], sohu["article_content_txt"]]
+        intervention_word_result = []
+        re_result = []
+        for i, word in enumerate(intervention_word_copy):
+            if "......" in word:
+                re_data = re.findall(
+                    word.replace("......", r"\w.{1,5}?"), sohu["remove_useless"]
+                )
+            else:
+                re_data = re.findall(word, sohu["remove_useless"])
+            if re_data:
+                re_result.extend(re_data)
+                intervention_word_result.append(intervention_word[i])
+        write_html_file(sohu["number"], sohu["article_content_txt"], re_result)
+        sohu_data.extend(
+            [
+                ",".join(list(set(intervention_word_result))),
+                len(list(set(intervention_word_result))),
+                len(intervention_word_result),
+                get_status(sohu["pos_status"], sohu["neg_status"]),
+                "./html_file/{}.html".format(sohu["number"]),
+            ]
+        )
+        result.append(sohu_data)
+    all_data.update({"sohu": result})
+
+    intervention_word_copy = intervention_word[:]
+    for tianya in data["tianya"]:
+        tianya_data = [tianya["number"], tianya["question_detail"]]
+        intervention_word_result = []
+        re_result = []
+        for i, word in enumerate(intervention_word_copy):
+            if "......" in word:
+                re_data = re.findall(
+                    word.replace("......", r"\w.{1,5}?"), tianya["remove_useless"]
+                )
+            else:
+                re_data = re.findall(word, tianya["remove_useless"])
+            if re_data:
+                re_result.extend(re_data)
+                intervention_word_result.append(intervention_word[i])
+        write_html_file(tianya["number"], tianya["question_detail"], re_result)
+        tianya_data.extend(
+            [
+                ",".join(list(set(intervention_word_result))),
+                len(list(set(intervention_word_result))),
+                len(intervention_word_result),
+                get_status(tianya["pos_status"], tianya["neg_status"]),
+                "./html_file/{}.html".format(tianya["number"]),
+            ]
+        )
+        result.append(tianya_data)
+    all_data.update({"tianya": result})
+    return all_data
+
+
+def get_status(pos_status, neg_status):
+    if not pos_status:
+        return "积极"
+    if not neg_status:
+        return "消极"
+
+
+def write_html_file(number, text, replace_color_text_list):
+    for replace_color_text in list(set(replace_color_text_list)):
+        text = text.replace(
+            replace_color_text, "<span>{}</span>".format(replace_color_text)
+        )
+    text = "<p>{}</p>".format(text)
+
+    html_text = (
+        """
+        <!DOCTYPE HTML>
+            <html>
+            <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+            <title>文件预览</title>
+            <style type="text/css">
+            span{
+               color:red;
+            }
+            </style>
+            </head>
+            <body>
+                %s
+            </body>
+            </html>
+        """
+        % text
+    )
+    with open(os.path.join("../resource/data/html_file/", number + ".html"), "w") as w:
+        w.write(html_text)
+
+
+def write_data_excel(data):
+    sina_data = data["sina"]
+    book_name_xls = "../resource/data/新浪介入词统计.xls"
+    write_excel_xls(  # noqa
+        book_name_xls,
+        "新浪介入词统计",
+        [["编号", "文本", "介入词", "介入词数量", "介入词出现次数", "类型", "预览地址"]],
+    )
+    write_excel_xls_append(book_name_xls, sina_data)  # noqa
+
+    sohu_data = data["sohu"]
+    book_name_xls = "../resource/data/搜狐介入词统计.xls"
+    write_excel_xls(  # noqa
+        book_name_xls,
+        "搜狐介入词统计",
+        [["编号", "文本", "介入词", "介入词数量", "介入词出现次数", "类型", "预览地址"]],
+    )
+    write_excel_xls_append(book_name_xls, sohu_data)  # noqa
+
+    tianya_data = data["tianya"]
+    book_name_xls = "../resource/data/天涯介入词统计.xls"
+    write_excel_xls(  # noqa
+        book_name_xls,
+        "天涯介入词统计",
+        [["编号", "文本", "介入词", "介入词数量", "介入词出现次数", "类型", "预览地址"]],
+    )
+    write_excel_xls_append(book_name_xls, tianya_data)  # noqa
+
+
+if __name__ == "__main__":
+    result = count_word(get_data(), get_txt_data())
+    write_data_excel(result)
--- utils/xlwt_util.py
+++ utils/xlwt_util.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-"""
-@Time    : 2019-02-26 20:12
-@Author  : red
-@Site    : 
-@File    : xlwt_util.py
-@Software: PyCharm
-"""
 import xlwt
 import csv
+
+from xlutils.copy import copy
+import xlrd
 
 """
 title = ['','','']
@@ -17,35 +12,58 @@ array = [['','',''], ['','',''], ['','','']...]
 
 
 def writeListData(data, name):
-    temp = [('file name', 'positive number', 'negative number', 'word count')]
+    temp = [("file name", "positive number", "negative number", "word count")]
     for element in data:
-        temp.append((element.get('number'), element.get('positive_number'), element.get('negative_number'), element.get('word_count')))
-    with open(name, 'a', errors='ignore', newline='') as f:
+        temp.append(
+            (
+                element.get("number"),
+                element.get("positive_number"),
+                element.get("negative_number"),
+                element.get("word_count"),
+            )
+        )
+    with open(name, "a", errors="ignore", newline="") as f:
         f_csv = csv.writer(f)
         f_csv.writerows(temp)
     # print('write_csv success')
 
 
 def writeListData1(data, name, table_type):
-    temp = [('file name', 'positive number', 'negative number', 'word count', str('order ' + table_type))]
+    temp = [
+        (
+            "file name",
+            "positive number",
+            "negative number",
+            "word count",
+            str("order " + table_type),
+        )
+    ]
     for element in data:
-        temp.append((element.get('number'), element.get('positive_number'), element.get('negative_number'), element.get('word_count'), element.get("order_" + table_type)))
-    with open(name, 'a', errors='ignore', newline='') as f:
+        temp.append(
+            (
+                element.get("number"),
+                element.get("positive_number"),
+                element.get("negative_number"),
+                element.get("word_count"),
+                element.get("order_" + table_type),
+            )
+        )
+    with open(name, "a", errors="ignore", newline="") as f:
         f_csv = csv.writer(f)
         f_csv.writerows(temp)
     # print('write_csv success')
-    
-    
+
+
 def writeData(data, name):
     temp = []
     for element in data:
-        if element.get('') != 0:
+        if element.get("") != 0:
             temp.append(element)
-    with open(name, 'a', errors='ignore', newline='') as f:
+    with open(name, "a", errors="ignore", newline="") as f:
         f_csv = csv.writer(f)
         f_csv.writerows(temp)
-        
-    
+
+
 def save_xlwt(col_num, sheet_name, title, array, save_path):
     excel_table = xlwt.Workbook()
     sheet1 = excel_table.add_sheet(sheet_name, cell_overwrite_ok=True)
@@ -57,5 +75,56 @@ def save_xlwt(col_num, sheet_name, title, array, save_path):
     excel_table.save(save_path)
 
 
-if __name__ == '__main__':
-    writeData([(1, 1), (2, 3)], "./tmp.csv")
+def write_excel_xls(path, sheet_name, value):
+    index = len(value)  # 获取需要写入数据的行数
+    workbook = xlwt.Workbook()  # 新建一个工作簿
+    sheet = workbook.add_sheet(sheet_name)  # 在工作簿中新建一个表格
+    for i in range(0, index):
+        for j in range(0, len(value[i])):
+            sheet.write(i, j, value[i][j])  # 像表格中写入数据（对应的行和列）
+    workbook.save(path)  # 保存工作簿
+    print("xls格式表格写入数据成功！")
+
+
+def write_excel_xls_append(path, value):
+    index = len(value)  # 获取需要写入数据的行数
+    workbook = xlrd.open_workbook(path)  # 打开工作簿
+    sheets = workbook.sheet_names()  # 获取工作簿中的所有表格
+    worksheet = workbook.sheet_by_name(sheets[0])  # 获取工作簿中所有表格中的的第一个表格
+    rows_old = worksheet.nrows  # 获取表格中已存在的数据的行数
+    new_workbook = copy(workbook)  # 将xlrd对象拷贝转化为xlwt对象
+    new_worksheet = new_workbook.get_sheet(0)  # 获取转化后工作簿中的第一个表格
+    for i in range(0, index):
+        for j in range(0, len(value[i])):
+            new_worksheet.write(
+                i + rows_old, j, value[i][j]
+            )  # 追加写入数据，注意是从i+rows_old行开始写入
+    new_workbook.save(path)  # 保存工作簿
+    print("xls格式表格【追加】写入数据成功！")
+
+
+if __name__ == "__main__":
+    book_name_xls = "xls格式测试工作簿.xls"
+
+    sheet_name_xls = "xls格式测试表"
+
+    value_title = [
+        ["姓名", "性别", "年龄", "城市", "职业"],
+    ]
+
+    value1 = [
+        ["张三", "男", "19", "杭州", "研发工程师"],
+        ["李四", "男", "22", "北京", "医生"],
+        ["王五", "女", "33", "珠海", "出租车司机"],
+    ]
+
+    value2 = [
+        ["Tom", "男", "21", "西安", "测试工程师"],
+        ["Jones", "女", "34", "上海", "产品经理"],
+        ["Cat", "女", "56", "上海", "教师"],
+    ]
+
+    # writeData([(1, 1), (2, 3)], "./tmp.csv")
+    write_excel_xls(book_name_xls, sheet_name_xls, value_title)
+    write_excel_xls_append(book_name_xls, value1)
+    write_excel_xls_append(book_name_xls, value2)
